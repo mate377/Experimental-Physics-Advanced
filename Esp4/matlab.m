d6060399cc08fb82a52ac7c92578ce8126a5b395
mate377 <matteo.finazzer@gmail.com>
2017-05-30 15:55:08 +0200

    print resolution of detector and commented the code

--- Esp4/matlab.m
+++ Esp4/matlab.m
@@ -1,4 +1,10 @@
 %% import dei dati
+% organizzo tutti i dati in due strutture:
+% 'sf' contiene i dati così come sono stati presi, mentre
+% 's' contiene i dato con fondo sottratto
+% 'maxsf' contiene i massimi delle serie col fondo, mi servono per
+%       tracciare le linee come ad esempio i 511Kev
+
 clear all;
 close all;
 sf=struct('sodio',load('sodio22.TKA'),'cobalto',load('cobalto60.TKA'),...
@@ -16,6 +22,8 @@ for i=1:length(fn)
     maxsf(i)=max(getfield(sf,fn{i}));
 end
 
+% Qui volevo aggiungere a tutti i dati 1 in modo che il log non dia
+% problemi.
 for i=1:5
     sf=setfield(sf,fn{i},getfield(sf,fn{i})+1);
 end
@@ -26,14 +34,18 @@ end
 
 
 %% calibrazione
+% 'ch' è il vettore contenente i canali
+% 'E' contiene invece le energie
+% la legge che mappa ch su E viene estratta tramite fit. spline sembra il
+% miglior tipo di fit.
+
 ch=1:2^12;
 calib.x=[1204 2909 3300]'; % se si vogliono aggiungere altri punti per la calibrazione
 calib.y=[511  1274 1505]'; % metterli in calib.x e calib.y. Si può anche cambiare il
 ft=fittype('a*x+b');    % tipo di fit in tal caso. calib.x contiene i canali
 f=fit(calib.x, calib.y, 'SmoothingSpline');% mentre calib.y le energie
 E=f(ch);
-
-%% mappatura dell'energia sui canali
+% plot mappatura dell'energia sui canali
 figure;
 plot(ch,E);
 title('map Energy to channel');
@@ -41,18 +53,20 @@ xlabel('Channel');
 ylabel('Energy');
 
 %% Variable for select log or semilogy plot
+% L'idea è questa: se linea=1 nei plot ci sarà la linea dei 511Kev,
+% altrimenti no.
 LINE=1;
 annihilation=511;
 linep=@(k) line([annihilation annihilation],[1 maxsf(k)],'Color','red'); 
 typeofplot='semilogy';
 %typeofplot='norm';
 
-G.msize=10;
+G.msize=3; % definisce la dimensione dei marker
 
 %% Plotta solo una sorgente
-N=1;
-close all;
-%E=ch; % temp for calibration
+N=3; % agendo su N seleziono quale grafico plottare. 5 è il fondo.
+close all; 
+% E=ch; % temp for calibration. Per quando voglio mappare ch -> E
 
 if (N<1||N>5)
     error('N tra 1 e 5 please!');
@@ -80,6 +94,7 @@ end
 title(sprintf('%s & fondo',fn{N}));
 xlabel('Energy [MeV]');
 ylabel('# counts');
+grid on;
 hold off;
 
 % e seconda senza fondo
@@ -102,6 +117,7 @@ end
 title(sprintf('%s - fondo',fn{N}));
 xlabel('Energy [MeV]');
 ylabel('# counts');
+grid on;
 hold off;
 
 %% tutti e 4 con fondo e plot anche del fondo
@@ -168,8 +184,42 @@ V.N=61000;
 %efficienza geometrica
 V.eg=(2.54*3)^2/(16*(25)^2);
 %attivit� stimata il 28/04/17
-V.Nt=V.N/(0.18*0.0058*1000);
-%TEMPO Trascoros
-V.T=-V.thalf*log(V.Nt/V.N0)/log(2);
+V.Nt=V.N/(0.18*V.eg*1000);
+%TEMPO Trascorso
+V.T=-V.thalf*log(V.Nt/V.N0)/log(2); % anni
 V.mesi=0.7*365;
 
+%% Risoluzione
+% calcolo la risoluzione trovando la larghezza della gaussiana di un picco.
+% Qui ho preso il sodio perchè è quello più bello. Fa anche un plot del fit
+% per assicurarsi che il risultato sia decente.
+
+R.int=2750:3050;
+R.para=log(sf.sodio(R.int));
+R.E=E(R.int);
+R.ft=fittype('-a*(x-b)^2+c');
+R.fit=fit(R.E,R.para,R.ft,'StartPoint',[1.4/(60^2) 1280 9]);
+% log(gauss) ~ -(x^2)/(2*sigma^2), so sigma=sqrt(1/(2*a)), but I need
+% 2*sigma which is the width of the gaussian.
+% Or I can use FWHM = 2.3548*sigma
+R.larg=sqrt(2/R.fit.a);
+R.FWHM=sqrt(1/(2*R.fit.a))*2.3548;
+%R.ris=R.larg/R.fit.b;
+R.ris=R.FWHM/R.fit.b;
+R.s=sprintf('la risoluzione del detector è circa %.2f%%', R.ris*100);
+disp(R.s);
+
+% plot
+close all;
+figure;
+plot(R.E,R.para,'.k');
+hold on;
+plot(R.E,R.fit(R.E),'r');
+xlabel('Energy [MeV]');
+ylabel('log(# counts)');
+title('fit parabola per trovare sigma');
+grid on;
+hold off;
+
+%% close all
+close all;
